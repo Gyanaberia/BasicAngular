ff4ee48179ba2942f058c116d3908046b704468e
Gyanaberia <gyanaprakashberia@gmail.com>
2020-10-18 13:06:21 +0530

    Updated form.Component

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -2,30 +2,61 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder,Validators} from '@angular/forms';
 //import { FormGroup, FormControl } from '@angular/forms';
 import { ServerService } from '../server.service';
+import { Formdata } from '../formdata';
+//import { Formdata } from '../formdata';
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
-  public formdata;
-  constructor(private fb:FormBuilder,private fdata:ServerService) { }
+  public formdata: Formdata;
+  successMsg="";
+  errorMsg="";
 
+  constructor(private fb:FormBuilder,private sservice:ServerService) {
+    this.sservice.getForm().subscribe(data => this.formdata = {
+                                                              name:data.name,
+                                                              feedback:data.feedback,
+                                                              email:data.email,
+                                                              comment:data.comment}); 
+    }  
   ngOnInit(){
-    this.fdata.getForm().subscribe(data => this.formdata = data);
   }
-  reviewForm=this.fb.group({//why is this required
+
+  public reviewForm=this.fb.group({//why is this required
     name:[''],
     email:[''],
+    feedback:[''],
     comment:[''],
   })
-loadApi(){    
-  this.reviewForm.patchValue({
-  name: this.formdata.name,
-  email:this.formdata.email,
-  //feedback:this.formdata.feedback,
-  comment:this.formdata.comment
-})
-}
-SubmitData(){}
+
+  loadApi(){    
+    this.reviewForm.patchValue({
+    name: this.formdata.name,
+    email:this.formdata.email,
+    feedback:this.formdata.feedback,
+    comment:this.formdata.comment})
+  }
+
+  SubmitData(){
+    this.errorMsg="",
+    this.successMsg="",
+    this.sservice.submitForm(this.reviewForm.value).subscribe(
+      response=>{this.successMsg="Your form has been successfully submitted!",
+                  console.log(response)},
+      error=>this.errorMsg="From submission Unsucessfull!!"+error.statusText,
+    )
+    // console.log(this.errorMsg);
+    // console.log(this.successMsg);
+    if (this.errorMsg==="") {
+     this.reviewForm.patchValue({
+      name: '',
+      email:'',
+      feedback:'',
+      comment:''
+    })}
+  }
+
 }
--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,16 +1,23 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Pipe,PipeTransform,Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Formdata } from './formdata';
-import { Observable } from 'rxjs';
+import { Observable,throwError } from 'rxjs';
+import {catchError,retry} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServerService {
-  private url="https://cs251-outlab-6.herokuapp.com/initial_values/"
+  gurl="https://cs251-outlab-6.herokuapp.com/initial_values/"
+  purl=" https://cs251-outlab-6.herokuapp.com/add_new_feedback/"
   constructor(private http:HttpClient) { }
   getForm():Observable<Formdata>{
-    return this.http.get<Formdata>(this.url);//returns an observable in the form of an Formdata.Formdata is a general interface in which the get data should be received
+    return this.http.get<Formdata>(this.gurl);//returns an observable in the form of an Formdata.Formdata is a general interface in which the get data should be received
   }
-  submitForm(){}
+  submitForm(userData:Formdata):Observable<Formdata>{
+    return this.http.post<Formdata>(this.purl,userData).pipe(catchError(this.errorhandler));
+  }
+  errorhandler(error:HttpErrorResponse){
+    return throwError(error);
+  }
 }
